a7a5db16b5ddf0faf00e6db8d1491c4b512d85a8
Karl Mulhall <karlmulhall@live.ie>
2022-05-04 21:38:16 +0100

    4/5/22 - Beginnings of text recognition page

--- app/src/main/java/com/example/kmregscan/MainActivity.java
+++ app/src/main/java/com/example/kmregscan/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.kmregscan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button captureImageButton, detectTextButton;
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureImageButton = findViewById(R.id.capture_image_button);
+        detectTextButton = findViewById(R.id.detect_text_button);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.text_view);
+
+        captureImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
     }
 }
+
